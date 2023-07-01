072991d6bf136f080a564443e566288da6114987
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-07-01 12:50:16 +0530

    Added solution - LeetHub

---
+++ String Manipulation - GFG/string-manipulation.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int removeConsecutiveSame(vector <string > v) 
+    {
+        // Your code goes here
+        stack<string> s;
+        for(auto itr:v)
+        {
+            if(s.empty())
+                s.push(itr);
+            else{
+                if(s.top()==itr)
+                    s.pop();
+                else
+                    s.push(itr);
+            }
+        }
+        return s.size();
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n;
+         cin>>n;
+         string s;
+         vector<string> v;
+         for(int i=0;i<n;i++)
+         {
+             cin>>s;
+             v.push_back(s);
+         }
+         Solution ob;
+         cout<< ob.removeConsecutiveSame(v) <<endl;
+     }
+	
+	
+	
+	
+	return 0;
+}
+// } Driver Code Ends
