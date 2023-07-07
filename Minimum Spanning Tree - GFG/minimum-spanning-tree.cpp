64abd21d3fb7362f8148f581fb1a435a0eae25c3
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-07-07 16:59:21 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -7,48 +7,89 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	
+	class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
+        vector<pair<int,pair<int,int>>> edges;
         
-        // vector<pair<int,int>> adj_list[V];
-        // first create the adjacency list
-        // for(auto itr:adj){
-        //     cout<<itr[0]<<" "itr[1]<<" "<<itr[2]<<endl;
-        // }
-        
-        // first create a min heap for distance and node
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        
-        // create a vis array
-        vector<int> vis(V,0);
-        
-        int MST_sum=0;
-        
-        while(!pq.empty())
-        {
-            auto itr=pq.top();
-            pq.pop();
-            
-            int dist=itr.first;
-            int node=itr.second;
-            
-            if(!vis[node])
-            {
-                vis[node]=1;
-                MST_sum+=dist;
-                // now look for adj nodes which are not visited
-                for(auto it:adj[node]){
-                    if(!vis[it[0]]){
-                        pq.push({it[1],it[0]});
-                    }
-                }
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int adjNode=it[0];
+                int wt=it[1];
+                int node=i;
+                
+                edges.push_back({wt,{node,adjNode}});
             }
         }
         
-        return MST_sum;
+        sort(edges.begin(),edges.end());
+        
+        int mst_weight=0;
+        DisjointSet st(V);
+        
+        for(auto it:edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            
+            if(st.findUPar(u) != st.findUPar(v)){
+                mst_weight+=wt;
+                st.unionBySize(u,v);
+            }
+        }
+        return mst_weight;
     }
 };
 
