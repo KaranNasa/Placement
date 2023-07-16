d920f08e48a9295f7476dd350160ebff32f33d3d
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-07-16 18:11:21 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -10,18 +10,15 @@ class Solution{
         void merge(long long arr1[], long long arr2[], int n, int m) 
         { 
             // code here 
-            
             int i=n-1,j=0;
-            
             while(i>=0 and j<m){
-                if(arr1[i] > arr2[j])
-                {
-                    swap(arr1[i],arr2[j]);
+                if(arr2[j] < arr1[i]){
+                    swap(arr2[j],arr1[i]);
                     i--;
                     j++;
                 }
                 else{
-                    break;
+                   break;
                 }
             }
             
