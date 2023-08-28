8eed25997279700c8f6ba42f391cd599340097c6
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-08-28 10:29:37 +0530

    Added solution - LeetHub

---
+++ Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+    if(head==NULL || head->next==NULL)
+        return head;
+        
+    Node *dummy=new Node(-1);
+    dummy->next=head;
+    Node *ptr=dummy;
+    unordered_map<int,int> m;
+    Node *p=head;
+    while(p!=NULL){
+        if(m.find(p->data)==m.end()){
+            ptr->next=p;
+            m[p->data]=1;
+            ptr=ptr->next;
+        }
+        else{
+            // p=p->next->next;
+        }
+        p=p->next;
+    }
+    ptr->next=NULL;
+    return dummy->next;
+}
