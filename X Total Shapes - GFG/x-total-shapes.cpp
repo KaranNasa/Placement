4a1cf63bdabc7460c5d3c2ef09ff012c06e7c58e
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-09-13 12:00:28 +0530

    Added solution - LeetHub

---
+++ X Total Shapes - GFG/x-total-shapes.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the number of 'X' total shapes.
+    void dfs(int i,int j,int n,int m,vector<vector<char>> &grid)
+    {
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j] == 'O')
+            return;
+        
+        grid[i][j]='O';
+        dfs(i+1,j,n,m,grid);
+        dfs(i-1,j,n,m,grid);
+        dfs(i,j+1,n,m,grid);
+        dfs(i,j-1,n,m,grid);
+        
+    }
+    int xShape(vector<vector<char>>& grid) 
+    {
+        // Code here
+        int count=0;
+        int n=grid.size(),m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 'X'){
+                    count++;
+                    dfs(i,j,n,m,grid);
+                }
+            }
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<char>>grid(n, vector<char>(m, '#'));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.xShape(grid);
+		cout << ans <<'\n';
+	}
+	return 0;
+}
+// } Driver Code Ends
