6d189589a47cda316e298d3bd7fa98ccd90ed1f9
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-09-14 15:58:10 +0530

    Added solution - LeetHub

---
+++ Sum of Leaf Nodes at Min Level - GFG/sum-of-leaf-nodes-at-min-level.cpp
@@ -0,0 +1,174 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+/*
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    int minLeafSum(Node *root)
+    {
+        //code here
+        if(root==NULL)
+            return 0;
+        
+        if(root->left==NULL and root->right==NULL)
+            return root->data;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        bool check=false;
+        while(!q.empty()){
+            int size=q.size();
+            int curr_sum=0;
+            while(size--){
+                Node *node=q.front();
+                q.pop();
+                
+                if(node->left==NULL and node->right==NULL){
+                    check=true;
+                    curr_sum+=node->data;
+                }
+                
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            if(check)
+                return curr_sum;
+        }
+        return 0;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        int res = obj.minLeafSum(root);
+        cout << res << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
