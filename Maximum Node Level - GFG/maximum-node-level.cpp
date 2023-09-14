d99cc175c1cd7f306da2cd3b3ab6e2b0b97817f4
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-09-14 15:38:24 +0530

    Added solution - LeetHub

---
+++ Maximum Node Level - GFG/maximum-node-level.cpp
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+int maxNodeLevel(Node *root);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        int ans = maxNodeLevel(root);
+        cout<<ans<<endl;
+    }
+    return 1;
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// Return the level (0-indexed) with maximum number of nodes.
+int maxNodeLevel(Node *root)
+{
+ // Add your code here
+    int max_level=-1,maxi=-1;
+    if(root->left==NULL and root->right==NULL)
+        return 0;
+    
+    queue<Node*> q;
+    int curr_level=0;
+    q.push(root);
+    while(!q.empty()){
+        int size=q.size();
+        if(size > max_level){
+            max_level=size;
+            maxi=curr_level;
+        }
+        // max_level=max(max_level,size)
+        while(size--){
+            Node *node=q.front();
+            q.pop();
+            
+            if(node->left)
+                q.push(node->left);
+            
+            if(node->right)
+                q.push(node->right);
+        }
+        curr_level++;
+    }
+    return maxi;
+}
