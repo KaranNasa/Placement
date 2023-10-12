c4a935703f3cd7f725fbfcf5e58d0ba3bb3dfcdd
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-10-12 18:31:59 +0530

    Added solution - LeetHub

---
+++ Maximum sum leaf to root path - GFG/maximum-sum-leaf-to-root-path.cpp
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    void solve(Node *root,int curr_sum,int &max_sum)
+    {
+        if(root==NULL)
+            return;
+        
+        if(root->left==NULL and root->right==NULL){
+            curr_sum+=root->data;
+            max_sum=max(max_sum,curr_sum);
+            return;
+        }
+        
+        curr_sum+=root->data;
+        
+        solve(root->left,curr_sum,max_sum);
+        solve(root->right,curr_sum,max_sum);
+    }
+    int maxPathSum(Node* root)
+    {
+        //code here
+       
+        if(root==NULL)
+            return 0;
+        if(root->left==NULL and root->right==NULL)
+            return root->data;
+        
+        int curr_sum=0;
+        int max_sum=INT_MIN;
+        
+        solve(root,curr_sum,max_sum);
+        
+        return max_sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.maxPathSum(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
