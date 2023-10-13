39916533d0f006ce0258c2ac608dbb1c9cc3b6ed
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-10-14 02:16:12 +0530

    Added solution - LeetHub

---
+++ Inorder Traversal and BST - GFG/inorder-traversal-and-bst.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        // code here
+        int i=0,j=1;
+        while(j<N){
+            if(arr[i]>=arr[j])
+                return false;
+            i++;
+            j++;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for(int i=0;i<N;i++)cin>>arr[i];
+        Solution ob;
+        cout<<ob.isRepresentingBST(arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
