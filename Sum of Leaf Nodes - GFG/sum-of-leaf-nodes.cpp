23e6e57cce22fe80b4edef1f89ce187241a0f9ae
Karan Nasa <71080919+KaranNasa@users.noreply.github.com>
2023-10-14 01:43:28 +0530

    Added solution - LeetHub

---
+++ Sum of Leaf Nodes - GFG/sum-of-leaf-nodes.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+int sumLeaf(Node* root);
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout<<sumLeaf(root)<<endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+/*Structure of the node of the binary tree is as
+struct Node
+{
+	int data;
+	Node *left, *right;
+};*/
+// function should return the sum of all the 
+// leaf nodes of the binary tree 
+void solve(Node *root,int &curr_sum)
+{
+    if(root==NULL)
+        return;
+    
+    if(root->left==NULL and root->right==NULL)
+        curr_sum+=root->data;
+    
+    solve(root->left,curr_sum);
+    solve(root->right,curr_sum);
+}
+int sumLeaf(Node* root)
+{
+    // Code here
+    if(root==NULL)
+        return 0;
+    if(root->left==NULL and root->right==NULL)
+        return root->data;
+    
+    int curr_sum=0;
+    solve(root,curr_sum);
+    return curr_sum;
+}
